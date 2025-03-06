474fe3b18f3a41ca804f5321c9bee223ec53e3ca
Matias Laime <matulai1221@gmail.com>
2025-03-06 16:06:06 -0300

    Fix: prettier warns

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import reactRefresh from "eslint-plugin-react-refresh";
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+import js from "@eslint/js";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintPluginPrettierRecommended],
+    extends: [
+      js.configs.recommended, 
+      ...tseslint.configs.recommended, 
+      eslintPluginPrettierRecommended, 
+      ...pluginQuery.configs['flat/recommended']
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2022,
@@ -24,6 +30,7 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+      'prettier/prettier': 0,
     },
   }
 );
