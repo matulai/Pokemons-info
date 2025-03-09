8bad240f2ab6433bebcd19dbf1ede89c5bdbcd53
Matias Laime <matulai1221@gmail.com>
2025-03-09 20:45:11 -0300

    Add: aux functions on utils

---
+++ src/utils/generateAlphabeticAccess.ts
@@ -0,0 +1,17 @@
+import { PokemonResponse } from '@/types';
+
+function generateAlphabeticAccess(data: PokemonResponse[]): Record<string, PokemonResponse[]> {
+  const groups: Record<string, PokemonResponse[]> = {};
+
+  for (const pokemon of data) {
+    const firstLetter = pokemon.name[0]!.toLowerCase();
+    if (!groups[firstLetter]) {
+      groups[firstLetter] = [];
+    }
+    groups[firstLetter].push(pokemon);
+  }
+
+  return groups;
+}
+
+export default generateAlphabeticAccess;
