37a73bca17e024bfd92f03ccf9a089c774f71751
Matias Laime <matulai1221@gmail.com>
2025-03-10 21:12:30 -0300

    Add: More icons

---
+++ src/icons/ChevronUpIcon.tsx
@@ -0,0 +1,23 @@
+interface ChevronUpIconProps {
+  color: string,
+}
+
+const ChevronUpIcon = ({ color }: ChevronUpIconProps) => {
+  return (
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      width="24" 
+      height="24" 
+      viewBox="0 0 24 24" 
+      fill="none" 
+      stroke={color} 
+      strokeWidth="2" 
+      strokeLinecap="round" 
+      strokeLinejoin="round" 
+      className="lucide lucide-chevron-up">
+        <path d="m18 15-6-6-6 6"/>
+      </svg>
+  )
+}
+
+export default ChevronUpIcon;
--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,1 +1,3 @@
 export { default as PlayIcon } from "./PlayIcon";
+export { default as ChevronUpIcon } from "./ChevronUpIcon";
+export { default as ChevronDownIcon } from "./ChevronDownIcon";
