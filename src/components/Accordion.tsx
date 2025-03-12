f03de660a3ac8961c91bd07ee4f4610dace987f1
Matias Laime <matulai1221@gmail.com>
2025-03-11 21:37:55 -0300

    Add: Accordion component

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,35 @@
+import { ChevronUpIcon, ChevronDownIcon } from "@/icons";
+import { useState } from "react";
+import { Button } from "@/components";
+
+interface AccordionProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const Accordion = ({ title, children }: AccordionProps) => {
+
+  const [showAboutContent, setShowAboutContent] = useState<boolean>(false);
+
+  return (
+    <div className="pokemon-about-container">
+      <div className="pokemon-about-container-header">
+        {title}
+        <Button 
+          children={showAboutContent 
+            ? <ChevronDownIcon color="yellow" />
+            : <ChevronUpIcon color="yellow" />} 
+          onClick={() => setShowAboutContent(!showAboutContent)}
+        />
+      </div>
+      <div className={showAboutContent? 
+          "pokemon-about-container-content"
+        : "pokemon-about-container-content no-visible"
+      }>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Accordion;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,5 @@
 export { default as Search } from "./Search";
 export { default as Button } from "./Button";
+export { default as Accordion } from "./Accordion";
+export { default as PokemonInformation } from "./PokemonInformation";
+export { default as PokemonTypes } from "./PokemonTypes";
