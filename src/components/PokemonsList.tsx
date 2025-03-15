dff388499074a37e8d0791c81b8825862f725051
Matias Laime <matulai1221@gmail.com>
2025-03-15 12:30:31 -0300

    Add: PokemonsList component

---
+++ src/components/PokemonsList.tsx
@@ -0,0 +1,22 @@
+import { SimplePokemon } from "@/types";
+import { Link } from "react-router-dom";
+
+interface PokemonsListProps {
+  pokemonsList: SimplePokemon[]
+}
+
+const PokemonsList = ({ pokemonsList }: PokemonsListProps) => {
+  return (
+    <ul className="pokemons-list">
+      {pokemonsList.map(pokemon => (
+        <li className="pokemons-list-pokemon" key={pokemon.name}>
+          <Link to={`/pokemon/${pokemon.url.match(/\/(\d+)\/$/)?.[1] ?? ""}`}>
+            {pokemon.name}
+          </Link>
+        </li>
+      ))}
+    </ul>    
+  )
+}
+
+export default PokemonsList;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 export { default as Search } from "./Search";
 export { default as Button } from "./Button";
 export { default as Accordion } from "./Accordion";
-export { default as PokemonInformation } from "./PokemonInformation";
 export { default as PokemonTypes } from "./PokemonTypes";
+export { default as PokemonsList } from "./PokemonsList";
+export { default as PokemonInformation } from "./PokemonInformation";
