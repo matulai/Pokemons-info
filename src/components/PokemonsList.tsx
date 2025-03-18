9bd6acc601080fe6505b54f0ac83a3fbedfbfb90
Matias Laime <matulai1221@gmail.com>
2025-03-18 00:08:16 -0300

    Add: Search options change to absolute position

--- src/components/PokemonsList.tsx
+++ src/components/PokemonsList.tsx
@@ -1,21 +1,24 @@
 import { SimplePokemon } from "@/types";
 import { Link } from "react-router-dom"
 
+import "@/styles/PokemonsList.css";
+
 interface PokemonsOptionsListProps {
   pokemonsList: SimplePokemon[];
 }
 
 const PokemonsList = ({pokemonsList}: PokemonsOptionsListProps) => {
   return(
-    <>
+    <ul className="pokemons-list">
       {pokemonsList.map(pokemon => (
         <li className="pokemons-list-pokemon" key={pokemon.name}>
+          <img src={`https://raw.githubusercontent.com/matulai/Pokemon-sprites/master/sprites/pokemon/${pokemon.url.match(/\/(\d+)\/$/)?.[1] ?? ""}.png`} alt={`${pokemon.name} image`}/>
           <Link className="pokemons-list-pokemon-link" to={`/pokemon/${pokemon.url.match(/\/(\d+)\/$/)?.[1] ?? ""}`}>
             {pokemon.name}
           </Link>
         </li>
       ))}
-    </>
+    </ul>
   )
 }
 
