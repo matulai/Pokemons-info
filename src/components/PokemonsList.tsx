7dd2a47f66766fd82f499d5621f5bf74a6334479
Matias Laime <matulai1221@gmail.com>
2025-03-18 23:42:39 -0300

    Add: List pokemons only show until the last image is load

--- src/components/PokemonsList.tsx
+++ src/components/PokemonsList.tsx
@@ -1,4 +1,5 @@
 import { SimplePokemon } from "@/types";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "@/styles/PokemonsList.css";
@@ -8,20 +9,28 @@ interface PokemonsOptionsListProps {
 }
 
 const PokemonsList = ({ pokemonsList }: PokemonsOptionsListProps) => {
-  console.log(pokemonsList)
+  const [allImagesLoads, setAllImagesLoads] = useState<boolean>(false);
+
+  function handleOnLoad(index: number) {
+    if (pokemonsList.length - 1 === index) {
+      setAllImagesLoads(true);
+    }
+  }
+
   return (
-    <ul className={`pokemons-list ${pokemonsList.length !== 0? "": "invisible"}`}>
-      {pokemonsList.map(pokemon => (
+    <ul className={`pokemons-list ${pokemonsList.length !== 0 && allImagesLoads? "": "invisible"}`}>
+      {pokemonsList.map((pokemon, index) => (
         <li className="pokemons-list-pokemon" key={pokemon.name}>
-          <img
-            className="pokemons-list-pokemon-image"
-            src={`https://raw.githubusercontent.com/matulai/Pokemon-sprites/master/sprites/pokemon/${pokemon.url.match(/\/(\d+)\/$/)?.[1] ?? ""}.png`}
-            alt={`${pokemon.name} image`}
-          />
           <Link
             className="pokemons-list-pokemon-link"
             to={`/pokemon/${pokemon.url.match(/\/(\d+)\/$/)?.[1] ?? ""}`}
           >
+            <img
+              className="pokemons-list-pokemon-image"
+              src={`https://raw.githubusercontent.com/matulai/Pokemon-sprites/master/sprites/pokemon/${pokemon.url.match(/\/(\d+)\/$/)?.[1] ?? ""}.png`}
+              alt={`${pokemon.name} image`}
+              onLoad={() => handleOnLoad(index)}
+            />
             {pokemon.name}
           </Link>
         </li>
