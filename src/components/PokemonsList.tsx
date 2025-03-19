9afc0ce46f20b3f0e83c2a4d29824ec655fb5cd2
Matias Laime <matulai1221@gmail.com>
2025-03-19 15:11:39 -0300

    Fix: Show images when all are loaded

--- src/components/PokemonsList.tsx
+++ src/components/PokemonsList.tsx
@@ -9,29 +9,40 @@ interface PokemonsOptionsListProps {
 }
 
 const PokemonsList = ({ pokemonsList }: PokemonsOptionsListProps) => {
-  const [allImagesLoads, setAllImagesLoads] = useState<boolean>(false);
+  const [loadedImages, setLoadedImages] = useState<number>(0);
+  const [prevList, setPrevList] = useState(pokemonsList);
 
-  function handleOnLoad(index: number) {
-    if (pokemonsList.length - 1 === index) {
-      setAllImagesLoads(true);
-    }
+  // Otra manera no se me ocurrio de resolver esto.
+  // Resetear el contador de imágenes si la lista cambia
+  if (prevList !== pokemonsList) {
+    setLoadedImages(0);
+    setPrevList(pokemonsList);
   }
 
+  console.log("pre", prevList, "actual", pokemonsList);
+  console.log("loadedImages", loadedImages, "pokemonsList", pokemonsList.length)
+
+  function handleOnLoad() {
+    setLoadedImages((prev) => prev + 1);
+  }
+
+  const allImagesLoads = pokemonsList.length > 0 && loadedImages === pokemonsList.length;
+  console.log(allImagesLoads);
   return (
-    <ul className={`pokemons-list ${pokemonsList.length !== 0 && allImagesLoads? "": "invisible"}`}>
-      {pokemonsList.map((pokemon, index) => (
+    <ul className={`pokemons-list ${allImagesLoads? "visible": ""}`}>
+      {pokemonsList.map(pokemon => (
         <li className="pokemons-list-pokemon" key={pokemon.name}>
           <Link
             className="pokemons-list-pokemon-link"
             to={`/pokemon/${pokemon.url.match(/\/(\d+)\/$/)?.[1] ?? ""}`}
           >
             <img
-              className="pokemons-list-pokemon-image"
+              className="pokemons-list-pokemon-link-image"
               src={`https://raw.githubusercontent.com/matulai/Pokemon-sprites/master/sprites/pokemon/${pokemon.url.match(/\/(\d+)\/$/)?.[1] ?? ""}.png`}
               alt={`${pokemon.name} image`}
-              onLoad={() => handleOnLoad(index)}
+              onLoad={handleOnLoad}
             />
-            {pokemon.name}
+            <p className="pokemons-list-pokemon-link-text">{pokemon.name}</p>
           </Link>
         </li>
       ))}
